7ad163e0ed9bae3e058c9dd647f0e06667195da0
Koushik <Koushikbajpayee06@gmail.com>
2025-12-26 23:12:01 +0530

    var, let, const in JavaScript

---
+++ 01_variables/01.variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = 'koushikbaju07@gmail.com';
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+// accountId = 2;
+console.log(accountId);
+
+accountEmail = 'Koushok@gmail.com';
+accountPassword= "2125262335";
+accountCity = "Bangalore";
+
+
+console.table([accountId, accountEmail, accountCity, accountState])
