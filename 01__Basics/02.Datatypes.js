1779221288eb6a0dc73a30fb6a97ab10029363b9
Koushik <Koushikbajpayee06@gmail.com>
2025-12-26 23:57:06 +0530

    conversionOperator

---
+++ 01__Basics/02.Datatypes.js
@@ -0,0 +1,11 @@
+let str = "Koushik";
+let abc = 15;
+let isBool = true;
+let xyz
+let pqr = null;
+
+console.table([typeof pqr, typeof xyz]);
+console.log(pqr==undefined);
+console.log(xyz==null);
+console.log(xyz == pqr);
+
---
+++ 01__Basics/03.conversionOperator.js
@@ -0,0 +1,13 @@
+let str1 = "hello";
+let str2 = "hitesh";
+
+let str3 = str1 + " " + str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log("1"+2 +2);
+console.log(1+2 +"2");
+let gameCounter = 100;
+console.log(gameCounter++)
+// gameCounter++;
+console.log(gameCounter);
